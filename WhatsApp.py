81be259ed4ab4c6c40673a702fe09d647f6b77fc
Felix Weichselgartner <felix.weichselgartner@t-online.de>
2023-12-21 13:43:35 +0100

    update to newest xpath

--- WhatsApp.py
+++ WhatsApp.py
@@ -64,9 +64,8 @@ class WhatsApp:
         :return: the search bar.
         """
 
-        try:          
-                                                    
-            ret = self.web.find_element("xpath", """/html/body/div[1]/div/div/div[4]/div/div[1]/div/div/div[2]/div/div[1]""")
+        try:
+            ret = self.web.find_element("xpath", """/html/body/div[1]/div/div[2]/div[3]/div/div[1]/div/div[2]/div[2]/div/div[1]""")
         except selenium.common.exceptions.NoSuchElementException:
             ret = None
         return ret
@@ -90,8 +89,7 @@ class WhatsApp:
         # <div class="wjdTm" style="visibility: visible;">Schreib eine Nachricht</div>
         # xpath:
         try:                                        
-            ret = self.web.find_element("xpath", """/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]""")          
-            #ret = self.web.find_element_by_xpath("""/html/body/div[1]/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]""")                                                    
+            ret = self.web.find_element("xpath", """/html/body/div[1]/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div[2]/div[1]""")            #ret = self.web.find_element_by_xpath("""/html/body/div[1]/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]""")                                                    
         except selenium.common.exceptions.NoSuchElementException:
             ret = None
         return ret
@@ -101,7 +99,7 @@ class WhatsApp:
 
         for i in range(15):
             try:
-                obj = self.web.find_element("xpath", f"/html/body/div[1]/div/div/div[4]/div/div[2]/div[1]/div/div/div[{i}]")
+                obj = self.web.find_element("xpath", f"/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[{i}]")
             except selenium.common.exceptions.NoSuchElementException:
                 continue
             if 'transform: translateY(72px)' in obj.get_attribute('style'):
